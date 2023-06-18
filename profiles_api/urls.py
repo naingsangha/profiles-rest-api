dc8515506b27e5bc007704f5bec7ca2fb2872b61
naingsangha <naing.seiha.hs@meoys.gov.kh>
2023-06-18 11:06:34 +0700

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 
-# from rest_framework.routers import DefaultRouter
-
-# router = DefaultRouter()
-# router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-
 from profiles_api import views
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viwset')
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
-     path('hello-view/', views.HelloApiView.as_view()), 
+     path('hello-view/', views.HelloApiView.as_view()),
+     path('login/', views.UserLoginApiView.as_view()),
+     path('', include(router.urls))
 ]
+ 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -14,3 +26,57 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    
+    def post(self, request):
+        """"Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+    
+    def path(self, request, pk=None):
+        """Handle a partial update of and object"""
+        return Response({'method': 'PATH'})
+    
+    def delete(self, request, pk=None):
+        """"Delete an object"""
+        return Response({'method': 'DELETE'})
+    
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create ,retrieve, update, partial, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email', )
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle crating user Authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
